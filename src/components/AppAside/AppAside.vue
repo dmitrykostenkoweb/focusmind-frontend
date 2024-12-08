d9de6701e97b28552aa312768dd87e65658f8a65
dmitrykostenkoweb <d.kostenko@pixel.com.pl>
2024-12-08 14:52:56 +0100

    wip

---
+++ src/components/AppAside/AppAside.vue
@@ -0,0 +1,17 @@
+<template>
+  <el-aside width="200px">
+    <el-scrollbar>
+      <el-menu :default-openeds="['1', '3']">
+        <app-aside-area />
+        <app-aside-project />
+        <app-aside-task />
+      </el-menu>
+    </el-scrollbar>
+  </el-aside>
+</template>
+
+<script lang="ts" setup>
+import { AppAsideArea, AppAsideProject, AppAsideTask } from './AppAsideNodes'
+</script>
+
+<style scoped></style>
