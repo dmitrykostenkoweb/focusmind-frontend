b7304d686960e2262f8a4dd980c33dac686ee92b
dmitrykostenkoweb <d.kostenko@pixel.com.pl>
2025-03-15 00:13:36 +0100

    Refactor styles and assets: moved main CSS to a new directory, removed unused base CSS, added new background SVG and styles, and updated CardComponent for improved styling.

--- src/components/common/CardComponent/CardComponent.vue
+++ src/components/common/CardComponent/CardComponent.vue
@@ -1,9 +1,9 @@
 <template>
-  <el-card :style="{ background, color }" class="entity-card" shadow="hover">
+  <el-card class="entity-card" shadow="hover">
     <template #header>
       <div class="entity-card__header">
         <p class="entity-card__name">{{ entity.name }}</p>
-        <el-button type="info" size="large" :icon="Edit" @click="emit('edit')" />
+        <el-button circle :icon="Edit" @click="emit('edit')" />
       </div>
     </template>
     <div class="entity-card__cover-wrapper">
@@ -27,7 +27,6 @@
 import { computed } from 'vue'
 import { Edit, PictureFilled } from '@element-plus/icons-vue'
 import type { Entity } from '@/models/entity.model'
-import { hexToRgba, isLightColor } from '@/utils'
 
 interface Props {
   entity: Entity
@@ -39,14 +38,6 @@ interface Emits {
 
 const { entity } = defineProps<Props>()
 const emit = defineEmits<Emits>()
-
-const background = computed<string>(() => (entity.hex ? hexToRgba(entity.hex, 0.5) : '#fff'))
-const color = computed(() => {
-  const match = background.value?.match(/\d+/g)?.map(Number)
-  if (!match || match.length < 3) return '#303133'
-  const [r, g, b] = match
-  return isLightColor(r, g, b) ? '#303133' : '#FFFFFF'
-})
 </script>
 
 <style scoped lang="scss">
@@ -75,6 +66,8 @@ const color = computed(() => {
   &__cover {
     width: 100%;
     height: 100%;
+    border-radius: 4px;
+    opacity: 0.8;
   }
 
   &__image-slot {
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,30 +1,5 @@
 import { ElMessage } from 'element-plus'
 
-export function hexToRgba(hex: string, alpha: number = 1): string {
-  hex = hex.replace(/^#/, '')
-
-  if (hex.length === 3) {
-    hex = hex
-      .split('')
-      .map((char) => char + char)
-      .join('')
-  }
-
-  if (hex.length !== 6) throw new Error('Nieprawidłowy format HEX')
-
-  const r = parseInt(hex.substring(0, 2), 16)
-  const g = parseInt(hex.substring(2, 4), 16)
-  const b = parseInt(hex.substring(4, 6), 16)
-
-  return `rgba(${r}, ${g}, ${b}, ${alpha})`
-}
-
-export function isLightColor(r: number, g: number, b: number): boolean {
-  const luminance = 0.2126 * r + 0.7152 * g + 0.0722 * b
-
-  return luminance > 128
-}
-
 export const showMessage = (message: string, type: 'success' | 'error' | 'info') => {
   ElMessage({ message, type, plain: true })
 }
