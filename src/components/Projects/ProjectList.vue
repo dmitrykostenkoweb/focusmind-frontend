4f2cd7b9352bafbcf2c240f8891ad82909154a95
dmitrykostenkoweb <d.kostenko@pixel.com.pl>
2025-04-19 23:42:41 +0200

    Refactor ProjectList component: Integrate ElCollapseItem for better organization, add StatusFilter for project status selection, and enhance project header with ElBadge for project count display.

--- src/components/Projects/ProjectList.vue
+++ src/components/Projects/ProjectList.vue
@@ -1,29 +1,49 @@
 <template>
-  <header class="project-list__header">
-    <h2>Projects</h2>
-    <el-button plain icon="Plus" size="small" @click="interactionStore.openCreateDialog('project')"
-      >Add new project</el-button
-    >
-  </header>
-  <div class="grid-container">
-    <card-component
-      v-for="entity in projectApiStore.projects"
-      :key="entity.id"
-      :entity
-      @edit="interactionStore.openEditDialog(entity, 'project')"
-    />
-  </div>
+  <el-collapse-item name="project">
+    <template #title>
+      <header class="project-list__header">
+        <h2>Projects <el-badge :value="projectApiStore.projects.length" /></h2>
+        <el-button
+          plain
+          icon="Plus"
+          size="small"
+          @click.stop="interactionStore.openCreateDialog('project')"
+          >Add new project</el-button
+        >
+      </header>
+    </template>
+    <status-filter v-model="selectedStatuses" :statuses="statuses" />
+    <div class="grid-container">
+      <card-component
+        v-for="entity in projectApiStore.projects"
+        :key="entity.id"
+        :entity
+        @edit="interactionStore.openEditDialog(entity, 'project')"
+      />
+    </div>
+  </el-collapse-item>
   <project-form v-if="interactionStore.entityType === 'project'" />
 </template>
 
 <script lang="ts" setup>
-import { onMounted } from 'vue'
+import { onMounted, ref, watch } from 'vue'
 import { useInteractionStore } from '@/stores/interaction/interactionStore'
 import { useProjectApiStore } from '@/stores/project/projectApiStore'
 import CardComponent from '@/components/common/CardComponent/CardComponent.vue'
 import ProjectForm from './ProjectForm.vue'
+import StatusFilter from '@/components/common/StatusFilter/StatusFilter.vue'
+import { statusesOptions } from '@/constants'
+import type { StatusOption, Status } from '@/models/shared.model'
+
 const projectApiStore = useProjectApiStore()
 const interactionStore = useInteractionStore()
+
+const selectedStatuses = ref<Status[]>(['InProgress'])
+const statuses = ref<StatusOption[]>(statusesOptions)
+
+watch(selectedStatuses, async (newStatuses) => {
+  await projectApiStore.fetchProjects(newStatuses)
+})
 
 onMounted(async () => await projectApiStore.fetchProjects())
 </script>
