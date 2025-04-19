b9523c49be1c59787ea6ce1f106486c27f01cb8d
dmitrykostenkoweb <d.kostenko@pixel.com.pl>
2025-04-19 23:42:48 +0200

    Refactor TaskList component: Integrate ElCollapseItem for better organization, add StatusFilter for task status selection, and enhance task header with ElBadge for task count display.

--- src/components/Tasks/TaskList.vue
+++ src/components/Tasks/TaskList.vue
@@ -1,30 +1,49 @@
 <template>
-  <header class="task-list__header">
-    <h2>Tasks</h2>
-    <el-button plain icon="Plus" size="small" @click="interactionStore.openCreateDialog('task')"
-      >Add new task</el-button
-    >
-  </header>
-  <div class="grid-container">
-    <card-component
-      v-for="entity in taskApiStore.tasks"
-      :key="entity.id"
-      :entity
-      @edit="interactionStore.openEditDialog(entity, 'task')"
-    />
-  </div>
+  <el-collapse-item name="task">
+    <template #title>
+      <header class="task-list__header">
+        <h2>Tasks <el-badge :value="taskApiStore.tasks.length" /></h2>
+        <el-button
+          plain
+          icon="Plus"
+          size="small"
+          @click.stop="interactionStore.openCreateDialog('task')"
+          >Add new task</el-button
+        >
+      </header>
+    </template>
+    <status-filter v-model="selectedStatuses" :statuses="statuses" />
+    <div class="grid-container">
+      <card-component
+        v-for="entity in taskApiStore.tasks"
+        :key="entity.id"
+        :entity
+        @edit="interactionStore.openEditDialog(entity, 'task')"
+      />
+    </div>
+  </el-collapse-item>
   <task-form v-if="interactionStore.entityType === 'task'" />
 </template>
 
 <script lang="ts" setup>
-import { onMounted } from 'vue'
+import { onMounted, ref, watch } from 'vue'
 import { useInteractionStore } from '@/stores/interaction/interactionStore'
 import { useTaskApiStore } from '@/stores/task/taskApiStore'
 import CardComponent from '@/components/common/CardComponent/CardComponent.vue'
 import TaskForm from './TaskForm.vue'
+import StatusFilter from '@/components/common/StatusFilter/StatusFilter.vue'
+import { statusesOptions } from '@/constants'
+import type { Status, StatusOption } from '@/models/shared.model'
 
 const taskApiStore = useTaskApiStore()
 const interactionStore = useInteractionStore()
+
+const selectedStatuses = ref<Status[]>(['InProgress'])
+const statuses = ref<StatusOption[]>(statusesOptions)
+
+watch(selectedStatuses, async (newStatuses) => {
+  await taskApiStore.fetchTasks(newStatuses)
+})
 
 onMounted(async () => await taskApiStore.fetchTasks())
 </script>
