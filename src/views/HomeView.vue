f180fe457c5d55e4922f2daae1cd3977b28e7b4f
dmitrykostenkoweb <d.kostenko@pixel.com.pl>
2025-04-19 23:43:00 +0200

    Refactor HomeView component: Integrate ElCollapse for better organization of area, project, and task lists, and initialize activeNames state for managing visibility.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,19 @@
 <template>
   <main>
-    <area-list />
-    <project-list />
-    <task-list />
+    <el-collapse v-model="activeNames">
+      <area-list />
+      <project-list />
+      <task-list />
+    </el-collapse>
   </main>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import AreaList from '@/components/Areas/AreaList.vue'
 import ProjectList from '@/components/Projects/ProjectList.vue'
 import TaskList from '@/components/Tasks/TaskList.vue'
+import type { EntityType } from '@/models/entity.model'
+
+const activeNames = ref<EntityType[]>(['area', 'project', 'task'])
 </script>
