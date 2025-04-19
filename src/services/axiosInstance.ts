c8b75d08c893aadedc4802ab33847288630bc87b
dmitrykostenkoweb <d.kostenko@pixel.com.pl>
2025-04-19 23:40:52 +0200

    Add qs library for query string serialization in axios instance: Update axiosInstance to use qs for parameter serialization, enhancing API request handling.

--- src/services/axiosInstance.ts
+++ src/services/axiosInstance.ts
@@ -1,10 +1,16 @@
-import axios, { type AxiosInstance } from "axios";
+import axios, { type AxiosInstance } from 'axios'
+import qs from 'qs'
 
 const axiosInstance: AxiosInstance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: 'http://localhost:3000',
   headers: {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
   },
-});
+  paramsSerializer: {
+    serialize: (params) => {
+      return qs.stringify(params, { arrayFormat: 'repeat' })
+    },
+  },
+})
 
-export default axiosInstance;
+export default axiosInstance
